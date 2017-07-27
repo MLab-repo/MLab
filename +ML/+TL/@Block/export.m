4e1e172e5c19f3b59c6001af7ec15c298ea6af27
Raphael Candelier <raphael.candelier@upmc.fr>
2017-07-27 17:54:43 +0200

    Finish the ML.LT.BLock class

--- +ML/+TL/@Block/export.m
+++ +ML/+TL/@Block/export.m
@@ -3,59 +3,11 @@ function out = export(this, varargin)
 % --- Inputs --------------------------------------------------------------
 
 in = ML.Input;
-in.filename{''} = 'char';
+in.filename = 'char,nonempty';
 in = in.process;
 
 % --- Built text ----------------------------------------------------------
 
-text = '';
-build(1,0);
-
-% --- Output --------------------------------------------------------------
-
-if nargout
-    out = text;
-end
-
-if ~isempty(in.filename)
-    fid = fopen(in.filename, 'w');
-    fprintf(fid, '%s', text);
-    fclose(fid);
-end
-
-% === NESTED FUNCTIONS ====================================================
-
-    function build(index, level)
-
-        switch this.Tree(index).type
-            
-            case 'Root'
-                text = [this.Tree(index).options newline];
-                build(index+1, level);
-                
-            case 'text'
-                text = [text repmat(this.tab, [1 level]) this.Tree(index).options newline];
-                
-            otherwise
-        
-                text = [text repmat(this.tab, [1 level]) ...
-                    '<' this.Tree(index).type];
-                
-                F = fieldnames(this.Tree(index).options);
-                for i = 1:numel(F)
-                    text = [text ' ' F{i} '=''' this.Tree(index).options.(F{i}) '''']; %#ok<*AGROW>
-                end
-                
-                text = [text '>' newline];
-                
-                for i = this.Tree(index).children
-                    build(i, level+1);
-                end
-                
-                text = [text repmat(this.tab, [1 level]) ...
-                    '</' this.Tree(index).type '>' newline];
-        end
-        
-    end
-
-end
+fid = fopen(in.filename, 'w');
+fprintf(fid, '%s', this.build);
+fclose(fid);
