03e01fb4a92c09efeb9530710e9947b2c57295a8
Raphaël Candelier <mlab@candelier.fr>
2017-07-29 11:03:58 +0200

    ML.TL.Xhtml: modify input gestion in  <base>

--- +ML/+TL/@Xhtml/base.m
+++ +ML/+TL/@Xhtml/base.m
@@ -3,13 +3,7 @@ function out = base(this, varargin)
 % --- Input ---------------------------------------------------------------
 in = ML.Input;
 in.href = 'char';
-in.target{''} = @(x) ismember(x, {'_self', '_blank', '_parent', '_top'});
-in = in.process;
+in = in.process('merge', true, 'OutputClass', 'cell');
 
 % --- Processing ----------------------------------------------------------
-
-if isempty(in.target)
-    in = rmfield(in, 'target'); 
-end
-
 out = this.single(this.Index.head, 'base', 'attributes', in);
